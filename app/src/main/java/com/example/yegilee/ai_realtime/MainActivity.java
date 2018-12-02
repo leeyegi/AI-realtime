a64a3e77dfe6fe515563a62ae9eb88c3c37755dd
yegilee <zxcasd312@naver.com>
2018-12-03 00:42:56 +0900

    tooth

--- app/src/main/java/com/example/yegilee/ai_realtime/MainActivity.java
+++ app/src/main/java/com/example/yegilee/ai_realtime/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.SystemClock;
+import android.support.v4.app.ActivityCompat;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -21,6 +22,8 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 
+import static java.security.AccessController.getContext;
+
 
 public class MainActivity  extends BlunoLibrary {
     //ui구성에 필요한 인스턴스
@@ -37,19 +40,27 @@ public class MainActivity  extends BlunoLibrary {
     Handler handler;
     int Seconds, Minutes, MilliSeconds;
 	boolean toothflag=false;
+	boolean arr1save=true;
+	boolean arr2save=false;
 
     //real-time에 필요한 인스턴스
-	private static final int N_SAMPLES = 50;
+	private static final int N_SAMPLES = 40;
 	private static List<Float> ax;
 	private static List<Float> ay;
 	private static List<Float> az;
 	private static List<Float> gx;
 	private static List<Float> gy;
 	private static List<Float> gz;
+	private static List<Float> ax2;
+	private static List<Float> ay2;
+	private static List<Float> az2;
+	private static List<Float> gx2;
+	private static List<Float> gy2;
+	private static List<Float> gz2;
 
 	//prediction 한 결과를 담는 인스턴스
 	private float[] results;
-	private float[] resultsTmp;
+	//private float[] resultsTmp;
 
 	private TensorFlowClassifier classifier;
 
@@ -85,6 +96,12 @@ public class MainActivity  extends BlunoLibrary {
 		gx = new ArrayList<>();
 		gy = new ArrayList<>();
 		gz = new ArrayList<>();
+		ax2 = new ArrayList<>();
+		ay2 = new ArrayList<>();
+		az2 = new ArrayList<>();
+		gx2 = new ArrayList<>();
+		gy2 = new ArrayList<>();
+		gz2 = new ArrayList<>();
 
 		//tensorflow 분류기 선언
 		classifier = new TensorFlowClassifier(getApplicationContext());
@@ -159,146 +176,183 @@ public class MainActivity  extends BlunoLibrary {
     //====================================
 
     //받아오는 데이터 처리하기 위한 인스턴스
-	Queue<String> queueSave=new LinkedList<String>();
 	StringBuilder  bufferStr=new StringBuilder ();
 	int index1=0;
 	int index2=0;
-	String drop;
+	String saveStr;
 	StringBuilder tmp;
 	String save[];
 
 	@Override
 	public void onSerialReceived(String theString) {                     //Once connection data received, this function will be called
 		// TODO Auto-generated method stub
-		//start time과 endtime을 시간을 측정하고싶은곳에 넣으면 됨
-		//long startTime = System.currentTimeMillis();
-		//long endTime = System.currentTimeMillis();
-		//textSerialReceived.append("timecheck onCreate:" + String.valueOf(endTime - startTime) + "\n");
 
 		bufferStr.append(theString);
 
-		//받은데이터가 20보다 크고 \n(구분자)가 존재하면 data save
-		if(bufferStr.lastIndexOf("\n")>1) {
-			//구분자가 있는 위치 받음
+		if(bufferStr.length()>25 && bufferStr.lastIndexOf("\n")>1) {
+
 			index2 = bufferStr.lastIndexOf("\n");
 
-			if (bufferStr.lastIndexOf("\n") < 45) {
+			//데이터 저장
+			saveStr = bufferStr.substring(index1, index2);
+			tmp = bufferStr.delete(0, index2);
 
-				//데이터 저장
-				save = bufferStr.substring(index1, index2).split(" ");
-				//이상한 데이터 확인하고 이상한 데이터 0으로 make
-				if (save.length >= 6) {
-					for (int i = 0; i < 6; i++) {
-						try { 							//이상한 데이터 모두 0으로 만듬
-							Float.parseFloat(save[i]);
-
-						} catch (NumberFormatException e) {
-							save[i] = "0";
-							Log.e("strange data","data -> 0\n");
-						}
+			save=saveStr.split(" ");
+			if (toothflag == true && save.length==6) {
+				if(arr1save==true) {
+					ax.add(Float.valueOf(save[0]));
+					ay.add(Float.valueOf(save[1]));
+					az.add(Float.valueOf(save[2]));
+					gx.add(Float.valueOf(save[3]));
+					gy.add(Float.valueOf(save[4]));
+					gz.add(Float.valueOf(save[5]));
 					}
-					if(toothflag==true) {
-						//이상한 데이터 처리 후 저장
-						ax.add(Float.valueOf(save[0]));
-						ay.add(Float.valueOf(save[1]));
-						az.add(Float.valueOf(save[2]));
-						gx.add(Float.valueOf(save[3]));
-						gy.add(Float.valueOf(save[4]));
-						gz.add(Float.valueOf(save[5]));
-						activityPrediction();
-					}
+				if(arr2save==true) {
+					ax2.add(Float.valueOf(save[0]));
+					ay2.add(Float.valueOf(save[1]));
+					az2.add(Float.valueOf(save[2]));
+					gx2.add(Float.valueOf(save[3]));
+					gy2.add(Float.valueOf(save[4]));
+					gz2.add(Float.valueOf(save[5]));
+					Log.e("toothflag2 save", String.valueOf(ax2.size()));
 				}
-			} else {
-				//버리는 데이터
-				drop = bufferStr.substring(index1, index2);
-				tmp = bufferStr.delete(0, index2);
-				Log.e("drop","data" + drop + "\n");
+				}
+			//}
 
+			if(gz.size()==20){
+				arr2save=true;
 			}
 
-			index1 = 1;
 
+			index1 = 1;
+			//각 사이즈가 50개씩 쌓이면 아래 코드 실행
+			if (gz.size() == N_SAMPLES) {
+				Log.e("toothflag save", String.valueOf(gz.size()));
+
+				activityPrediction(ax, ay, az, gx, gy, gz);
+				ax.clear();
+				ay.clear();
+				az.clear();
+				gx.clear();
+				gy.clear();
+				gz.clear();
+			}
+			if (gz2.size() == N_SAMPLES) {
+				Log.e("toothflag save", String.valueOf(gz2.size()));
+
+				activityPrediction(ax2, ay2, az2, gx2, gy2, gz2);
+				ax2.clear();
+				ay2.clear();
+				az2.clear();
+				gx2.clear();
+				gy2.clear();
+				gz2.clear();
+			}
 		}
 
+
 		//The Serial data from the BLUNO may be sub-packaged, so using a buffer to hold the String is a good choice.
-		//((ScrollView)textSerialReceived.getParent()).fullScroll(View.FOCUS_DOWN);
+		//((ScrollView)toothState.getParent()).fullScroll(View.FOCUS_DOWN);
 	}
 
     //====================================
     //데이터를 받아 50개씩 모은 후 신경망에서 평가하는 메소드
     //====================================
-	private void activityPrediction() {
-		//각 사이즈가 50개씩 쌓이면 아래 코드 실행
-		if (ax.size() == N_SAMPLES && ay.size() == N_SAMPLES && az.size() == N_SAMPLES) {
+	private void activityPrediction(List<Float> arrax, List<Float> array, List<Float> arraz, List<Float> arrgx, List<Float> arrgy, List<Float> arrgz) {
 
+		//리스트에 각 값을 저장
+		List<Float> data = new ArrayList<>();
+		data.addAll(arrax);
+		data.addAll(array);
+		data.addAll(arraz);
 
-			//리스트에 각 값을 저장
-			List<Float> data = new ArrayList<>();
-			data.addAll(ax);
-			data.addAll(ay);
-			data.addAll(az);
+		data.addAll(arrgx);
+		data.addAll(arrgy);
+		data.addAll(arrgz);
 
-			data.addAll(gx);
-			data.addAll(gy);
-			data.addAll(gz);
+		//prediction한 class의 probability를 배열에 담아줌
+		results = classifier.predictProbabilities(toFloatArray(data));
+		float[] resultsTmp=new float[results.length];
+		System.arraycopy(results, 0,resultsTmp, 0,results.length);
 
-			//prediction한 class의 probability를 배열에 담아줌
-			results = classifier.predictProbabilities(toFloatArray(data));
-			resultsTmp = classifier.predictProbabilities(toFloatArray(data));
+		//prabability가 낮은 순으로 소팅 수행
+		Arrays.sort(resultsTmp);
 
-			Log.e("toothState", String.valueOf(results));
-
-			//prediction한 결과 뿌려줌
-			//textSerialReceived.append("results" + results + "\n");
-			//Log.e("toothState", String.valueOf(results.length));
-
-			//float[] resultTmp={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-			//resultTmp=results;
-
-			//prabability가 낮은 순으로 소팅 수행
-			Arrays.sort(resultsTmp);
-
-			toothState.setText(" ");
-			for(int i=0;i<16;i++)
-			{
-				if(i%2==0) {
-					//toothState.append(String.valueOf(i+1 +" -> "+results[i])+" ");
-					toothState.append(String.format("%2d -> %.10f \t", i + 1, results[i]));
-					toothState.append(String.format("%2d -> %.10f \n", i + 2, results[i+1]));
-				}
-				if(resultsTmp[15]==results[i]){
-					int rank=i+1;
-					Log.e("rank", String.valueOf(rank));
-					if(rank==1) toothImage.setImageResource(R.drawable.class1);
-					else if(rank==2) toothImage.setImageResource(R.drawable.class2);
-					else if(rank==3) toothImage.setImageResource(R.drawable.class3);
-					else if(rank==4) toothImage.setImageResource(R.drawable.class4);
-					else if(rank==5) toothImage.setImageResource(R.drawable.class5);
-					else if(rank==6) toothImage.setImageResource(R.drawable.class6);
-					else if(rank==7) toothImage.setImageResource(R.drawable.class7);
-					else if(rank==8) toothImage.setImageResource(R.drawable.class8);
-					else if(rank==9) toothImage.setImageResource(R.drawable.class9);
-					else if(rank==10) toothImage.setImageResource(R.drawable.class10);
-					else if(rank==11) toothImage.setImageResource(R.drawable.class11);
-					else if(rank==12) toothImage.setImageResource(R.drawable.class12);
-					else if(rank==13) toothImage.setImageResource(R.drawable.class13);
-					else if(rank==14) toothImage.setImageResource(R.drawable.class14);
-					else if(rank==15) toothImage.setImageResource(R.drawable.class15);
-					else if(rank==16) toothImage.setImageResource(R.drawable.class16);
-				}
-			}
-
-
-			//각 배열 clear
-			ax.clear();
-			ay.clear();
-			az.clear();
-			gx.clear();
-			gy.clear();
-			gz.clear();
+		toothState.setText(" ");
+		for(int i=0;i<16;i+=2)
+		{
+			//toothState.append(String.valueOf(i+1 +" -> "+results[i])+" ");
+			toothState.append(String.format("%2d -> %.10f \t", i + 1, results[i]));
+			toothState.append(String.format("%2d -> %.10f \n", i + 2, results[i+1]));
 		}
-	}
 
+		if(resultsTmp[15]==results[0]) {
+			toothImage.setImageResource(R.drawable.class1);
+			toothState.append("\nclass 1 \n");
+		}
+		else if(resultsTmp[15]==results[1]) {
+			toothImage.setImageResource(R.drawable.class2);
+			toothState.append("\nclass 2 \n");
+		}
+		else if(resultsTmp[15]==results[2]) {
+			toothImage.setImageResource(R.drawable.class3);
+			toothState.append("\nclass 3 \n");
+		}
+		else if(resultsTmp[15]==results[3]) {
+			toothImage.setImageResource(R.drawable.class4);
+			toothState.append("\nclass 4 \n");
+		}
+		else if(resultsTmp[15]==results[4]) {
+			toothImage.setImageResource(R.drawable.class5);
+			toothState.append("\nclass 5 \n");
+		}
+		else if(resultsTmp[15]==results[5]) {
+			toothImage.setImageResource(R.drawable.class6);
+			toothState.append("\nclass 6 \n");
+		}
+		else if(resultsTmp[15]==results[6]) {
+			toothImage.setImageResource(R.drawable.class7);
+			toothState.append("\nclass 7 \n");
+		}
+		else if(resultsTmp[15]==results[7]) {
+			toothImage.setImageResource(R.drawable.class8);
+			toothState.append("\nclass 8 \n");
+		}
+		else if(resultsTmp[15]==results[8]) {
+			toothImage.setImageResource(R.drawable.class9);
+			toothState.append("\nclass 9 \n");
+		}
+		else if(resultsTmp[15]==results[9]) {
+			toothImage.setImageResource(R.drawable.class10);
+			toothState.append("\nclass 10 \n");
+		}
+		else if(resultsTmp[15]==results[10]) {
+			toothImage.setImageResource(R.drawable.class11);
+			toothState.append("\nclass 11 \n");
+		}
+		else if(resultsTmp[15]==results[11]) {
+			toothImage.setImageResource(R.drawable.class12);
+			toothState.append("\nclass 12 \n");
+		}
+		else if(resultsTmp[15]==results[12]) {
+			toothImage.setImageResource(R.drawable.class13);
+			toothState.append("\nclass 13 \n");
+		}
+		else if(resultsTmp[15]==results[13]) {
+			toothImage.setImageResource(R.drawable.class14);
+			toothState.append("\nclass 14 \n");
+		}
+		else if(resultsTmp[15]==results[14]) {
+			toothImage.setImageResource(R.drawable.class15);
+			toothState.append("\nclass 15 \n");
+		}
+		else if(resultsTmp[15]==results[15]) {
+			toothImage.setImageResource(R.drawable.class16);
+			toothState.append("\nclass 16 \n");
+		}
+
+	}
+/*
 	private void showToothImage(float[] results, float[] resultsTmp){
 
 		for(int i=0;i<resultsTmp.length;i++){
@@ -325,11 +379,11 @@ public class MainActivity  extends BlunoLibrary {
 					else if(rank==14) toothImage.setImageResource(R.drawable.class14);
 					else if(rank==15) toothImage.setImageResource(R.drawable.class15);
 					else if(rank==16) toothImage.setImageResource(R.drawable.class16);
-				}*/
+				}
 		}
 
 	}
-
+*/
 	private float[] toFloatArray(List<Float> list) {
 		int i = 0;
 		float[] array = new float[list.size()];
